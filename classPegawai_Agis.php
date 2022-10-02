84983ebe3538e6177d052553634ced3b33a55e80
Agis Suryana <80836747+Agis1055@users.noreply.github.com>
2022-10-02 23:45:55 +0700

    Add files via upload

---
+++ classPegawai_Agis.php
@@ -0,0 +1,83 @@
+<?php
+class Pegawai{
+    //member 1 variabel
+    public $nip, $nama, $jabatan, $agama, $status, $gapok, $tunjab, $tunkel, $zakatprofes, $gakot, $gaber;
+
+    //member 2 konstruktor
+    public function __construct($nip, $nama, $jabatan, $agama, $status, $gapok, $tunjab, $tunkel, $zakatprofes, $gakot, $gaber)
+    {
+        $this->nip = $nip;
+        $this->nama = $nama;
+        $this->jabatan = $jabatan;
+        $this->agama = $agama;
+        $this->status = $status;
+        $this->gapok = $gapok;
+        $this->tunjab = $tunjab;
+        $this->tunkel = $tunkel;
+        $this->zakatprofes = $zakatprofes;
+        $this->gakot = $gakot;
+        $this->gaber = $gaber;
+    }
+
+    //member 3 method
+    private function setGajiPokok($jabatan, $gapok){
+        $this->gapok = $gapok;
+        switch ($jabatan){
+            case "manager":
+                $gapok = 15000000;
+                break;
+            case "asmen":
+                $gapok = 10000000;
+                break;
+            case "kabag":
+                $gapok = 7000000;
+                break;
+            case "staff":
+                $gapok = 4000000;
+                break;
+            default:
+                $gapok = 0;
+        }
+    }
+    
+    private function setTunjab($tunjab, $gapok){
+        $this->tunjab = $tunjab;
+        $tunkel = 0.20 * $gapok;
+    }
+
+    private function setTunkel($tunkel, $gapok, $status){
+        $this->tunkel = $tunkel;
+        if($status = 'menikah')
+        $tunkel = 0.10 * $gapok;
+        else $tunkel = 0;
+    }
+
+    private function setGakot($gapok, $tunjab, $tunkel, $gakot){
+        $this->gakot = $gakot;
+        $gakot = $gapok + $tunjab + $tunkel;
+    }
+    private function setZakatProfesi($zakatprofes, $gapok, $agama, $gakot){
+        $this->zakatprofes = $zakatprofes;
+        if($agama = 'islam' && $gakot >= 4000000)
+        $zakatprofes = 0.025 * $gapok;
+        else $zakatprofes = 0;
+    }
+    private function setGaber($gakot, $gaber, $zakatprofes){
+        $this->gaber = $gaber;
+        $gaber = $gakot - $zakatprofes;
+    }
+
+    public function cetak(){
+        echo 'NIP : '.$this->nip;
+        echo 'Nama : '.$this->nama;
+        echo 'Jabatan : '.$this->jabatan;
+        echo 'Agama : '.$this->agama;
+        echo 'Status : '.$this->status;
+        echo 'Gaji Pokok : '.$this->gapok;
+        echo 'Tunjangan Jabatan : '.$this->tunjab;
+        echo 'Tunjangan Keluarga : '.$this->tunkel;
+        echo 'Zakat : '.$this->zakatprofes;
+        echo 'Gaji Bersih : '.$this->gaber;
+    }
+}
+?>
---
+++ objPegawai_Agis.php
@@ -0,0 +1,6 @@
+<?php
+require 'classPegawai_Agis.php';
+//ciptakan objek
+$peg1 = new Pegawai('1209310239', 'Agis Suryana', 'manager', 'islam', 'tidak menikah');
+
+?>
